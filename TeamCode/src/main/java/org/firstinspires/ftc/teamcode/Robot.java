76fedb37f75045d1d8fef9bbf728419758c7e785
Cland29 <carl.lee.landskron@gmail.com>
2020-08-11 18:43:57 -0400

    Add Robot Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Robot {
+    public void robotInit(){
+
+    }
+
+    public void robotPeriodic(){
+
+    }
+
+    public void autonomousInit(){
+
+    }
+
+    public void autonomousPeriodic(){
+
+    }
+
+    public void teleopInit(){
+
+    }
+
+    public void teleopPeriodic(){
+
+    }
+}
